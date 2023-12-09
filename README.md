69e586f7f7a1a97ca7b42fb04140ba0a00d063c1
Omar Khaled <o.khaled6789@gmail.com>
2023-12-09 15:01:03 +0200

    refactored code, and updated README

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# Super-heterodyne_Receiver
+# Super-heterodyne Receiver
+
 simulateing an AM modulator and a corresponding super-heterodyne receiver using radio-station generated signals.
+![System Diagram](/images/system.png)
+
+## Modulating Signal Analysis
+
+This directory contains MATLAB code for analyzing modulating signals, including reading audio files, calculating signal spectra, generating carrier signals, and mixing them to produce Amplitude Modulated (AM) signals.
+
+### Prerequisites
+
+Before running the code, ensure that you have MATLAB installed on your system. The code utilizes the built-in functions provided by MATLAB for signal processing.
+
+### Usage
+
+1. **Clear Workspace:** Execute the following commands to clear the MATLAB workspace and close any open figures.
+
+    ```matlab
+    clear all;
+    close all;
+    ```
+
+2. **Read Modulating Signals:** Define a list of modulating signals (audio files) in the `modulating_signals` array. The code will loop through each signal for analysis.
+
+    ```matlab
+    modulating_signals = {"Short_BBCArabic2.wav", "Short_FM9090.wav", %...etc};
+    ```
+
+3. **Loop Through Modulating Signals:** The code iterates through each modulating signal, performs signal analysis, and generates AM signals.
+
+    ```matlab
+    for i = 1:length(modulating_signals)
+        % ... (code within the loop)
+    end
+    ```
+
+4. **Function: plot_spectrum:** The function `plot_spectrum` is defined to plot the spectrum of a given signal and calculate relevant information such as peaks, bandwidth, and BW. The function returns these calculated values.
+
+    ```matlab
+    function [peaks, bandwidth, BW] = plot_spectrum(k, FS, spectrum_L, signal_spectrum, title_str)
+        % ... (function code)
+    end
+    ```
+
+### Signal Analysis Process
+
+1. **Reading Modulating Signals:** Audio files are read, and their sampling frequency is obtained.
+
+2. **Converting to Single Channel:** Two-channel stereo signals are converted to a single channel stream by adding the two channels.
+
+3. **Increasing Sampling Frequency:** Sampling frequency is increased by interpolating the signal to conform to the Nyquist criteria for carrier signals.
+
+4. **Calculating Spectrum:** FFT is applied to the modulating signal to obtain its baseband bandwidth. The spectrum is then plotted.
+
+5. **Generating Carrier Signals:** Carrier signals are generated for each modulating signal with varying frequencies.
+
+6. **Mixing Signals:** Modulating signals are mixed with their respective carrier signals by multiplication.
+7. **Plotting AM Signals:** AM signals are plotted in separate figures.
+
+### Function Description
+
+The `plot_spectrum` function is responsible for plotting the spectrum of a given signal and calculating relevant information such as peaks, bandwidth, and BW. It returns these calculated values for further use.
+
+```matlab
+function [peaks, bandwidth, BW] = plot_spectrum(k, FS, spectrum_L, signal_spectrum, title_str)
+    % ... (function code)
+end
--- Super_heterodyne_Receiver.m
+++ Super_heterodyne_Receiver.m
@@ -1,82 +1,66 @@
-%%Reading the modulating signals and getting the sampling frequency.
-modulating_signal_1="Short_BBCArabic2.wav";  
-modulating_signal_2="Short_FM9090.wav";  
+%% Clearing workspace
+clear all;
+close all;
 
-[stereo_signal_1, FS_1] = audioread(modulating_signal_1);
-[stereo_signal_2, FS_2] = audioread(modulating_signal_2);
+%% Reading the modulating signals and getting the sampling frequency.
+%% List of modulating signals
+modulating_signals = {"Short_BBCArabic2.wav", "Short_FM9090.wav"};
 
-%%Converting from two channel stereo signal to single channel srtream by
-%%adding the two channels (the two columns) of each modulating signal.
-single_channel_1=stereo_signal_1(:,1)+stereo_signal_1(:,2);
-single_channel_2=stereo_signal_2(:,1)+stereo_signal_2(:,2);
-
-%%Increasing the sampling frequency by increasing the no. of samples to
-%%conform the Nyquist criteria for the carrier signals.
-single_channel_1=interp(single_channel_1,10);
-single_channel_2=interp(single_channel_2,10);
-
-%%Getting the FFT of the modulating signal to get it's Baseband bandwidth.
-%%%First message
-signal_spectrum_1=fft(single_channel_1);
-spectrum_L_1=length(signal_spectrum_1);
-k_1=-spectrum_L_1/2:spectrum_L_1/2-1;                          %%Adjusting the Axis scale so the signal spectrum is 
-subplot(2,1,1)                                                 %%plotted versus frequncy centered at zero to get the BW of the positive side of the spectrum.
-plot(k_1*10*FS_1/spectrum_L_1,fftshift(abs(signal_spectrum_1)))   
-title("Signal 1 spectrum")                      
-xlabel("freq. (Hz)")
-ylabel("Magnitude")
-
-%%%Second message
-signal_spectrum_2=fft(single_channel_2);          
-spectrum_L_2=length(signal_spectrum_2);
-k_2=-spectrum_L_2/2:spectrum_L_2/2-1; 
-subplot(2,1,2)
-plot(k_2*10*FS_2/spectrum_L_2,fftshift(abs(signal_spectrum_2)))
-title("Signal 2 spectrum")                      
-xlabel("freq. (Hz)")
-ylabel("Magnitude")
+%% Loop through each modulating signal
+for i = 1:length(modulating_signals)
+    %% Reading the modulating signal and getting the sampling frequency
+    [stereo_signal, FS] = audioread(modulating_signals{i});
+    %% Converting from two channel stereo signal to single channel srtream by
+    %% adding the two channels (the two columns) of each modulating signal.
+    single_channel = stereo_signal(:, 1) + stereo_signal(:, 2);
+    %% Increasing the sampling frequency by increasing the no. of samples to
+    %% conform the Nyquist criteria for the carrier signals.
+    single_channel = interp(single_channel, 10);
 
 
-%%Getting the BW 
-%%%BW of the first message
-threshold_dB = -3;  
-threshold = 10^(threshold_dB / 20);  
-peaks_1 = find(abs(signal_spectrum_1) > max(abs(signal_spectrum_1)) * threshold);
-bandwidth_1 = max(k_1(peaks_1));
-BW_1=bandwidth_1*10*FS_1/spectrum_L_1;
-%%%BW of the second message
-peaks_2 = find(abs(signal_spectrum_2) > max(abs(signal_spectrum_2)) * threshold);
-bandwidth_2 = max(k_2(peaks_2));
-BW_2=bandwidth_2*10*FS_2/spectrum_L_2;
+    %% Getting the FFT of the modulating signal to get its Baseband bandwidth.
+    signal_spectrum = fft(single_channel);
+    spectrum_L = length(signal_spectrum);
+     %% Adjusting the Axis scale so the signal spectrum is
+    %% plotted versus frequncy centered at zero to get the BW of the positive side of the spectrum.
+    k = -spectrum_L/2:spectrum_L/2-1;
 
+    [peaks, bandwidth, BW] = plot_spectrum(k, FS, spectrum_L, signal_spectrum, ['Signal ', num2str(i), ' Spectrum']);
+    %% Generating carrier signals
+    Fc = 100000 + (i - 1) * 55000;  % Adjust frequency for each signal
+    Ts = 1/(10 * FS);
+    L = length(single_channel);
+    t = (0:L-1) * Ts;
+    message_carrier = cos(2 * pi * Fc * t);
 
-%%Generating the carrier signals.
-  %%%Carrier of the first message(n=0).
-Fc_1=100000;
-Ts_1=1/(10*FS_1);                              %%Getting the Ts to Generate the carrier with the same sampling frequency of the message.
-L_1=length(single_channel_1);                  %%Getting the length of the message signal to generate the carreir with the same no. of samples.
-t_1=(0:L_1-1)*Ts_1;
-message_1_carrier=cos(2*pi*Fc_1*t_1);
+    %% Mixing the modulating signal with the carrier signal by multiplying them.
+    AM_signal = single_channel .* message_carrier';
 
-  %%%Carrier of the second message(n=1).
-Fc_2=155000;
-Ts_2=1/(10*FS_2);
-L_2=length(single_channel_2);
-t_2=(0:L_2-1)*Ts_2;
-message_2_carrier=cos(2*pi*Fc_2*t_2);
-
-%%Mixing the modulating signal with the carrier signal by multiplying them.
-AM_signal_1=single_channel_1 .* message_1_carrier';
-figure;
-subplot(2,1,1)
-plot(t_1,AM_signal_1)
-title("AM signal 1")                      
-xlabel("time")
-ylabel("Amplitude")
-
-AM_signal_2=single_channel_2 .* message_2_carrier';
-subplot(2,1,2)
-plot(t_2,AM_signal_2)
-title("AM signal 2")                      
-xlabel("time")
-ylabel("Amplitude")
+    %% Create a new figure for each AM signal
+    figure;
+    %% Plotting AM signal
+    plot(t, AM_signal);
+    title(['AM Signal ', num2str(i)]);
+    xlabel('Time');
+    ylabel('Amplitude');
+end
+   
+%% Function: plot_spectrum
+%% Description:
+    %% This function plots the spectrum of a given signal and calculates
+    %% relevant information such as peaks, bandwidth, and BW.
+%% Return: peaks, bandwidth, and BW.
+function [peaks, bandwidth, BW] = plot_spectrum(k, FS, spectrum_L, signal_spectrum, title_str)
+    threshold_dB = -3;
+    threshold = 10^(threshold_dB / 20);
+    peaks = find(abs(signal_spectrum) > max(abs(signal_spectrum)) * threshold);
+    bandwidth = max(k(peaks));
+    BW = bandwidth * 10 * FS / spectrum_L;
+    
+    figure;
+    plot(k * 10 * FS / spectrum_L, fftshift(abs(signal_spectrum)));
+    title(title_str);
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude');
+    fprintf('%s Bandwidth: %.2f Hz\n', title_str, BW);
+end
